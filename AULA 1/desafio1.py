c56c22ec2a34d553839a6d56e8712dbef930c8ab
Rafael Oliveira <oliver.enterprising@gmail.com>
2026-09-05 17:08:05 -0300

    Add files via upload

---
+++ AULA 1/desafio1.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+st.title("Meu Cartão de Visitas")
+
+st.header("Rafael Oliveira")
+
+st.text("Analista de TI Sênior")
+
+st.header("Sobre mim")
+
+st.text(
+    "Profissional de Tecnologia da Informação com experiência "
+    "em infraestrutura, redes, servidores e transformação digital."
+)
+
+st.header("Minhas habilidades")
+
+st.markdown("""
+- Infraestrutura de TI
+- Redes
+- Servidores
+- Cloud Computing
+- Python
+- Transformação Digital
+""")
+
+st.header("Contato: 11982714597")
+
+st.markdown("**E-mail: oliver.enterprising@email.com")
+st.markdown("**LinkedIn: https://www.linkedin.com/in/rafael-oliveira-b47b14189/")
